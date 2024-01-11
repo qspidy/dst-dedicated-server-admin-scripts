3991c837bc7fe1b93ea05b894c9da42a02c0a6b7
Albert <alspidyisthebest@gmail.com>
2024-01-11 21:44:49 +0800

    fix send_preset announce

--- dst_update_modlist.sh
+++ dst_update_modlist.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 # check paras
-if [[ "$1" == "0" ]]; then
+if [[ "$1" == "" ]]; then
   echo "Please set cluster serial number."
+  echo "Usage: $0 <cluster serial number>"
+  echo "    E.g: "
+  echo "        If the save directory is Cluster_5, run"
+  echo "        "$0 5""
   exit 1
 fi
 
 
 cluster_modlist_path=$HOME/.klei/DoNotStarveTogether/Cluster_$1/Master/modoverrides.lua
 server_modlist_path=$HOME/steamapps/DST/mods/dedicated_server_mods_setup.lua
+
 
 while read -r number; do
   echo "ServerModSetup(\"$number\")"
